92eac32d18649a6633abc29707b30b42a1f0c7c5
Wolfcodes91 <Dave.M.Wolfberg@gmail.com>
2022-01-27 23:36:26 -0800

    finished

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var guitars = require('../data/guitars')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { guitars });
 });
+
+router.post('/guitars', function(req, res) {
+  req.body.inTune = false;
+  guitars.push(req.body);
+  res.redirect('/')
+})
+
+router.delete('/guitars/:idx', function(req, res) {
+  guitars.splice(req.params.idx, 1)
+  res.redirect('/')
+})
 
 module.exports = router;
